428f68571016e98f7d9511814bd15f767c0f3c06
felipe-fcarvalho <felipefcarvalho7@gmail.com>
2026-07-18 21:37:34 -0300

    initial commit

---
+++ src/exercicio_ts.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function multiplica(n1, n2) {
+    return n1 * n2;
+}
+console.log(multiplica(10, 20));
+function dizOi(nome) {
+    return `Olá ${nome}`;
+}
+console.log(dizOi('Felipe'));
---
+++ src/exercicio_ts.ts
@@ -0,0 +1,11 @@
+function multiplica(n1: number, n2: number): number {
+    return n1 * n2;
+}
+
+console.log(multiplica(10, 20));
+
+function dizOi(nome: string): string {
+    return `Olá ${nome}`;
+}
+
+console.log(dizOi('Felipe'));
